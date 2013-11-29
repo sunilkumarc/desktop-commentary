a35dab9e6d5335e9df0c5087e0c908af03e1e5dd
Sunil Kumar <sunilc932@gmail.com>
2013-11-29 22:27:53 +0530

    Added icon picture to the notification bubble

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ sudo easy_install pip
 sudo pip install BeautifulSoup4
 sudo pip install lxml
 sudo apt-get install python-qt4
-sudo chmod +x remove.sh commentary.py script.sh error.sh stop.sh
+sudo chmod +x remove.sh commentary.py script.sh error.sh stop.sh icon.jpg
 sudo mkdir /opt/desktop-commentary
-sudo cp remove.sh commentary.py gui.py script.sh error.sh stop.sh /opt/desktop-commentary
+sudo cp remove.sh commentary.py gui.py script.sh error.sh stop.sh icon.jpg /opt/desktop-commentary
 sudo ln -s /opt/desktop-commentary/commentary.py /usr/bin/commentary
 sudo ln -s /opt/desktop-commentary/remove.sh /usr/bin/commentary-uninstall
 sudo ln -s /opt/desktop-commentary/stop.sh /usr/bin/stopcommentary
--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 commentary=$1
-notify-send -t 5000 "ScoreCard" "$commentary"
+notify-send -t 5000 "ScoreCard" "$commentary" -i /opt/desktop-commentary/icon.jpg
