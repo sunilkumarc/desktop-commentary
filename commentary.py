dda3227c6ee453a29c3a65fe2f533c9bc6bc2e44
Sunil Kumar <sunilc932@gmail.com>
2014-11-17 05:48:16 +0530

    Fixed the code for new espncricinfo layout

--- commentary.py
+++ commentary.py
@@ -5,9 +5,9 @@ import gui
 import sys
 import os
 import threading
-from bs4 import BeautifulSoup
-from urllib3 import PoolManager
+import urllib.request
 import threading
+import json
 
 class Score(QtGui.QMainWindow):
 	link = ""
@@ -28,66 +28,25 @@ class Score(QtGui.QMainWindow):
 		self.link = str(self.ui.scoreurl.toPlainText())
 		self.link = self.link.split("\x00")[0]
 		strings = self.link.split("/")
-		self.match_no = strings[len(strings)-1:][0]
+		self.match_no = strings[len(strings)-1:][0].split(".")[0]
 		self.close()
 		self.get_commentary()
 
 	def get_commentary(self):
-		m = PoolManager(10)
-		html = m.request('GET', 'http://www.espncricinfo.com/netstorage/710301.html')
-		soup = BeautifulSoup(html.data, "lxml")
-		tab = soup.find("table", "commsTable")
-		l = tab.findAll("p")
-		comm = ""
-		try:
-		    
-		    over = str(l[0]).split("<p class=\"commsText\">")[1].split("</p>")[0]
-		    over = float(over)
-		    comm = str(l[1]).split("<p class=\"commsText\">")[1].split("</p>")[0]
-		    #comm = l.split("<p class=\"commsText\">")[1].split("</p>")[0]
-		    try:
-		        c = comm.split("<span class=\"commsImportant\">")
-		        c1 = c[1].split("</span>")
-		        comm = c[0] + c1[0] + c1[1]
-		    except:
-		        pass
-		    comm = str(over) + ' - ' + str(comm)
-		except:
-		    comm = str(l)
-		    try:
-		        comm = comm.split("\">")[1].split("</p>")[0]
-		    except:
-		        temp = comm.split("\"")
-		        comm = temp[0] + temp[1]
+		url = "http://www.espncricinfo.com/netstorage/" + self.match_no +".json?xhr=1"
+		page = urllib.request.urlopen(url).read().decode("utf-8")
+		page = json.loads(page)
+		each_ball = page["comms"][0]["ball"][0]
+		self.commentary = ""
+		self.commentary = "Ind(288/7, 48.4) Vs Sri(286/8, 50)" + '\n'
+		players = page["centre"]["batting"]
+		player1 = players[0]
+		player2 = players[1]
 
-		string = comm.split('\n')
-		comm = ""
-		for line in string:
-		    comm += line
+		self.commentary += ''.join((player1["known_as"], " - ", player1["runs"],'(', player1["balls_faced"] ,')')) + "\n"
+		self.commentary += ''.join((player2["known_as"], " - ", player2["runs"],'(', player2["balls_faced"] ,')')) + "\n"
+		self.commentary += ''.join((each_ball["overs_actual"], " : ", each_ball['players'],',',each_ball['event'], ',', each_ball["text"]))
 
-		i = 1
-		one_line = 50
-		line_len = 50
-		length = len(comm)
-		while one_line < length:
-		    comm = comm[:(i*line_len)] + '\n' + comm[(i*line_len):]
-		    i += 1
-		    one_line += line_len
-
-		link = 'http://www.espncricinfo.com/ci/engine/match/710301.html'
-		m = PoolManager(10)
-		html = m.request('GET', link)
-		source = str(html.data)
-		page = source.split("<div class=\"topFrameTitle\">", 2)
-		page2 = page[1].split("</div>")
-		page3 = page2[0].split("data-text=\"")
-		page4 = page3[1].split("\">Tweet")
-		score = page4[0]
-		split_scores = score.split(" v ")
-		first_team = split_scores[0]
-		second_team = split_scores[1]
-
-		self.commentary = first_team + "\n" + second_team + "\n-------------------------------" +"\n" + comm
 		os.system("/opt/desktop-commentary/./script.sh" + " " + "\"" + self.commentary + "\"")
 		threading.Timer(30, self.get_commentary).start()
 
